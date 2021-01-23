a4643f4fd9ead30a40b046705399eb5e89b1973e
Vinicius Dias <vinicius.dias@achievers.com>
2021-01-23 17:15:42 -0300

    Video 5.3

--- Leilao/tests/teste-avaliador.cpp
+++ Leilao/tests/teste-avaliador.cpp
@@ -89,3 +89,11 @@ TEST_CASE("Leilão não deve receber lances consecutivos do mesmo usuário") {
     REQUIRE(1 == leilao.recuperaLances().size());
     REQUIRE(1000 == leilao.recuperaLances()[0].recuperaValor());
 }
+
+TEST_CASE("Usuário deve saber informar seu primeiro nome") {
+    Usuario vinicius("Vinicius Dias");
+
+    std::string primeiroNome = vinicius.recuperaPrimeiroNome();
+
+    REQUIRE("Vinicius" == primeiroNome);
+}
