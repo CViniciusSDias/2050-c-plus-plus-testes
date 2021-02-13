698b44d8cee0a22fe60f43a5e21a0888ce06140b
Vinicius Dias <vinicius.dias@achievers.com>
2021-02-13 13:10:17 -0300

    Video 2.1

--- Leilao/main.cpp
+++ Leilao/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include "Usuario.hpp"
 
 void* operator new(size_t bytes)
 {
@@ -22,6 +23,12 @@ int main() {
     ExibeNome(meuNome);
     ExibeNome(nomeEsposa);
     ExibeNome("Um nome qualquer sem estar na heap");
+    
+    Usuario* usuario = new Usuario("Vinicius Dias");
+    
+    std::cout << usuario->recuperaNome() << std::endl;
+
+    delete usuario;
 
     return 0;
 }
